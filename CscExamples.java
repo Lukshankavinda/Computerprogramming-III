2065af7ee896a7000d259f66aaabab3331cf8c08
Lukshankavinda <lukshankavindarathnayaka@gmail.com>
2020-09-18 04:49:59 +0600

    create CscExample java file

---
+++ CscExamples.java
@@ -0,0 +1,28 @@
+public class CscExample{
+	private int questionNo;
+	private String category;
+	private String text ;
+	
+	public void setQuestionNo(int questionNo){
+		
+	}
+	
+	public void setCategory(String category){
+		
+	}
+	public void setText(String text){
+		
+	} 
+	
+	public int getQuestionNo(){
+		
+	}
+	
+	public String getCategory(){
+		
+	}
+	public String getText(){
+		
+	} 
+	
+}
